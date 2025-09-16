f09ccda936be4c0430552863d92c69d98b2a412c
unknown <me@travishughes.net>
2025-09-16 04:49:43 +0100

    fixed bugs

--- Depoly/control-plane.sh
+++ Depoly/control-plane.sh
@@ -2,10 +2,10 @@
 
 # microk8s enable community
 microk8s enable dashboard
-microk8s enable cert-manager:10.64.140.43-10.64.140.49,192.168.0.105-192.168.0.111
+microk8s enable cert-manager
 microk8s enable registry
 microk8s enable rbac
-microk8s enable metallb
+microk8s enable metallb:10.64.140.43-10.64.140.49,192.168.0.105-192.168.0.111
 
 microk8s kubectl get all --all-namespaces
 
