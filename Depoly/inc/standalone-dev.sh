ddf80e5e77b6c90b607984c82e039c796fe0050f
unknown <me@travishughes.net>
2025-09-17 01:16:45 +0100

    added miniio to dev

--- Depoly/inc/standalone-dev.sh
+++ Depoly/inc/standalone-dev.sh
@@ -3,12 +3,13 @@ HOST_PUBLIC_IP=$(tailscale ip -4 | head -n1)
 microk8s disable ha-cluster --force
 
 microk8s enable hostpath-storage
-microk8s enable cert-manager
 microk8s enable rbac
-microk8s enable dashboard
-microk8s enable dashboard-proxy
+# microk8s enable cert-manager
+microk8s enable dns:"$DNS_SERVER"
 microk8s enable metallb:"${HOST_PUBLIC_IP}-${HOST_PUBLIC_IP}"
 microk8s enable miniio # TODO: Add these options -c 100Gi -s ceph-xfs
+microk8s enable dashboard
+microk8s enable dashboard-proxy
 
 # Ingress Controller
 kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
