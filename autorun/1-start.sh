4f21840d9ff937f52c692673ac99e59caa52220b
unknown <me@travishughes.net>
2025-09-20 00:42:50 +0100

    added code test hiding SSH_KEY echo

--- autorun/1-start.sh
+++ autorun/1-start.sh
@@ -4,8 +4,10 @@ HOSTNAME=$( ensure_var_defined "Hostname" $HOSTNAME )
 
 echo "Enter some details for your user, the password will be specifed later on."
 USERNAME=$( ensure_var_defined "Username" $USERNAME )
-SSH_KEY=$( ensure_var_defined "SSH Key" $SSH_KEY )
 
+stty -echo
+SSH_KEY=$( ensure_var_defined "SSH Key" $SSH_KEY )
+stty
 
 # echo "Please specify an email (used for system reporting)"
 # EMAIL=$( input_field "System Reporting Email" )
---
+++ autorun/3-setup-host.sh
@@ -0,0 +1,71 @@
+# Create user
+echo "\n Creating new users \n"
+
+add_user()
+{
+    L_USERNAME=$1
+    L_GROUP=$2
+
+    echo "Adding Username: $L_USERNAME"
+
+    sudo adduser --gecos "" --ingroup "$L_GROUP" "$L_USERNAME"
+    mkdir -p /home/"$L_USERNAME"/.ssh
+    # echo "$SSH_KEY" > /home/"$F_USERNAME"/.ssh/authorized_keys
+    echo "$SSH_KEY" | sudo tee /home/"$L_USERNAME"/.ssh/authorized_keys > /dev/null
+
+    sudo chown -R "$L_USERNAME":$L_GROUP /home/"$L_USERNAME"/.ssh
+    sudo chmod 700 /home/"$L_USERNAME"/.ssh
+    sudo chmod 600 /home/"$L_USERNAME"/.ssh/authorized_keys
+}
+
+add_user "$USERNAME-admin" sudo
+add_user "$USERNAME" users
+
+
+# if [ $USER = root ]; then
+#     echo "Disabling root user access"
+#     RANDOM_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
+#     usermod --password $(echo RANDOM_PASS | openssl passwd -1 -stdin) root
+#     echo "" > "~/.ssh/authorized_keys"
+# else
+#     echo "Please set a strong password for the current account."
+#     passwd
+#     echo "" > "~/.ssh/authorized_keys"
+# fi
+
+
+echo "\n Disabling SSH password authentication... \n"
+rm -f /etc/ssh/sshd_config.d/*
+mkdir -p /etc/ssh/sshd_config.d
+echo "PasswordAuthentication no" > /etc/ssh/sshd_config.d/disable-password-auth.conf
+
+
+
+echo "Setting system hostname and timezone..."
+hostnamectl set-hostname "$HOSTNAME"
+timedatectl set-timezone Europe/London
+
+# Install and configure Tailscale
+echo "Installing Tailscale..."
+curl -fsSL https://tailscale.com/install.sh | bash
+tailscale up --accept-risk=all
+
+
+echo "Cron Jobs"
+# Add cron job to reboot at 6:00 AM
+(crontab -l 2>/dev/null; echo "0 6 * * * /sbin/reboot") | crontab -
+
+
+# Update packages and install required software
+echo "Updating system and installing necessary packages..."
+apt update && apt upgrade -y
+apt install -y fail2ban ufw
+
+# Setup UFW
+echo "Enabling UFW firewall..."
+ufw allow OpenSSH
+ufw --force enable
+
+# Enable and start services
+echo "Enabling services..."
+systemctl enable fail2ban --now
