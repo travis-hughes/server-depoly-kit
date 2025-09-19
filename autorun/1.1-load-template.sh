ccc534f95c2bfc7b8807e86d09a41dd0b8f1fdf5
unknown <me@travishughes.net>
2025-09-19 06:45:53 +0100

    implementing fix

--- autorun/1.1-load-template.sh
+++ autorun/1.1-load-template.sh
@@ -27,7 +27,7 @@ wget -P ./deploy_tmp/template "https://k8s-deploy.ap-host.net/templates/$TEMPLAT
 
 
 if [ -e "./deploy_tmp/template/files.txt" ]; then
-    wget -P ./deploy_tmp/template -i ./deploy_tmp/template/files.txt -B "$FILE_URL/templates/$TEMPLATE"
+    wget -P ./deploy_tmp/template -i ./deploy_tmp/template/files.txt -B "https://k8s-deploy.ap-host.net/templates/$TEMPLATE"
 fi
 
 # Run template input script to collect specific inputs
