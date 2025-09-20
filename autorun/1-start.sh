7196dcfd804cc8e6a4f5d969992c691d3737e72b
unknown <me@travishughes.net>
2025-09-20 04:01:11 +0100

    fixed stty bug

--- autorun/1-start.sh
+++ autorun/1-start.sh
@@ -7,9 +7,10 @@ HOSTNAME=$( ensure_var_defined "Hostname" $HOSTNAME )
 echo "Enter some details for your user, the password will be specifed later on."
 USERNAME=$( ensure_var_defined "Username" $USERNAME )
 
+stty_orig=`stty -g`
 stty -echo
 SSH_KEY=$( ensure_var_defined "SSH Key" $SSH_KEY )
-stty echo
+stty $stty_orig
 
 # echo "\n========================================================================================\n"
 # echo "Select a server template: \n"
