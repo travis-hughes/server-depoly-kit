66761407fcc86d3f6bed4e7a4b995a0e20ca4dec
unknown <me@travishughes.net>
2025-09-20 03:55:39 +0100

    Removed tmux (for now)

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # For Ubuntu 24.04 LTS
 # Published URL: https://k8s-deploy.ap-host.net/run.sh
 
-EXACUTION_DIR=$(dirname $0)
+# EXACUTION_DIR=$(dirname $0)
 TEMP_DATA_PATH=/srv/deploy_tmp
 FILE_URL=https://k8s-deploy.ap-host.net
 
@@ -18,10 +18,10 @@ fi
 mkdir -p "$TEMP_DATA_PATH"
 
 # If deploy.env exists, load it.
-# if [ -e "deploy.env" ]; then
-#   echo "Environment Variables have been detected, loading them..."
-#   . ./deploy.env
-# fi
+if [ -e "deploy.env" ]; then
+  echo "Environment Variables have been detected, loading them..."
+  . ./deploy.env
+fi
 
 
 # Download dependancy files
@@ -30,21 +30,23 @@ wget -q -P "$TEMP_DATA_PATH" -i "$TEMP_DATA_PATH/files.txt" -B "$FILE_URL"
 
 
 # Run tmux if not already running
-if [ -z "$TMUX" ]; then
-  echo "🔁 Relaunching inside tmux session: deploy_session"
-  tmux new-session -s deploy_session -e "$EXACUTION_DIR" -e "$TEMP_DATA_PATH" -e "$FILE_URL" "$TEMP_DATA_PATH/session.sh"
-else
-  tmux attach deploy_session
-  # exit
-fi
+# if [ -z "$TMUX" ]; then
+#   echo "🔁 Relaunching inside tmux session: deploy_session"
+#   if tmux has-session -t deploy_session 2>/dev/null; then
+#     tmux attach -t deploy_session
+#   else
+#     tmux new-session -s deploy_session "bash $TEMP_DATA_PATH/session.sh"
+#   fi
+#   exit
+# fi
 
-# # Load scripts from autorun folder
-# for FILE in "$TEMP_DATA_PATH"/*-*.sh; do
-#   echo "\n ▶️ Exacuting autorun script: $FILE \n"
-#   . "$FILE"
-# done
+# Load scripts from autorun folder
+for FILE in "$TEMP_DATA_PATH"/*-*.sh; do
+  echo "\n ▶️ Exacuting autorun script: $FILE \n"
+  . "$FILE"
+done
 
-# # Cleanup and reboot
-# rm "$TEMP_DATA_PATH"
-# rm deploy.env
-# reboot
+# Cleanup and reboot
+rm "$TEMP_DATA_PATH"
+rm deploy.env
+reboot
