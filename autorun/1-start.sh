acd3495985caf0fb640ffa1225d8ca7fb2137a84
unknown <me@travishughes.net>
2025-09-20 04:07:28 +0100

    changed select code for test

--- autorun/1-start.sh
+++ autorun/1-start.sh
@@ -7,25 +7,25 @@ HOSTNAME=$( ensure_var_defined "Hostname" $HOSTNAME )
 echo "Enter some details for your user, the password will be specifed later on."
 USERNAME=$( ensure_var_defined "Username" $USERNAME )
 
-# stty_orig=`stty -g`
-# stty -echo
+stty_orig=`stty -g`
+stty -echo
 SSH_KEY=$( ensure_var_defined "SSH Key" $SSH_KEY )
-# stty $stty_orig
+stty $stty_orig
 
-# echo "\n========================================================================================\n"
-# echo "Select a server template: \n"
-# echo "1) Kubernetes (Experimental)"
-# echo "2) Docker"
-# echo "\n========================================================================================\n"
+echo "\n========================================================================================\n"
+echo "Select a server template: \n"
+echo "1) Kubernetes (Experimental)"
+echo "2) Docker"
+echo "\n========================================================================================\n"
 
-# read -p "Option (1/2): " option
-# case "$option" in
-#     1 ) TEMPLATE="k8s" ;;
-#     2 ) TEMPLATE="docker" ;;
-#     * ) echo "Invalid input, try again." && exit 1 ;;
-# esac
+read -p "Option (1/2): " option
+case "$option" in
+    1 ) TEMPLATE="k8s" ;;
+    2 ) TEMPLATE="docker" ;;
+    * ) echo "Invalid input, try again." && exit 1 ;;
+esac
 
-$TEMPLATE = $(select_field "Select a server template:" "k8s:Kubernetes (Experimental)" "docker:Docker")
+# $TEMPLATE = $(select_field "Select a server template:" "k8s:Kubernetes (Experimental)" "docker:Docker")
 
 # echo "Please specify an email (used for system reporting)"
 # EMAIL=$( input_field "System Reporting Email" )
