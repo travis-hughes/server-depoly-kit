5ffa0a849b084c2798f89fd16407902af7d003b1
unknown <me@travishughes.net>
2025-09-20 03:38:47 +0100

    added fix to tmux

--- run.sh
+++ run.sh
@@ -17,17 +17,16 @@ fi
 # Create Tempory Folder
 mkdir -p "$TEMP_DATA_PATH"
 
-
-# # Download dependancy files
-wget -q -P "$TEMP_DATA_PATH" "$FILE_URL/files.txt"
-wget -q -P "$TEMP_DATA_PATH" -i "$TEMP_DATA_PATH/files.txt" -B "$FILE_URL"
-
-
 # If deploy.env exists, load it.
 # if [ -e "deploy.env" ]; then
 #   echo "Environment Variables have been detected, loading them..."
 #   . ./deploy.env
 # fi
+
+
+# Download dependancy files
+wget -q -P "$TEMP_DATA_PATH" "$FILE_URL/files.txt"
+wget -q -P "$TEMP_DATA_PATH" -i "$TEMP_DATA_PATH/files.txt" -B "$FILE_URL"
 
 
 # Run tmux if not already running
--- session.sh
+++ session.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# If deploy.env exists, load it.
+# if [ -e "$EXACUTION_DIR/deploy.env" ]; then
+#   echo "Environment Variables have been detected, loading them..."
+#   . ./deploy.env
+# fi
+
 # Load scripts from autorun folder
 for FILE in "$TEMP_DATA_PATH"/*-*.sh; do
   echo "\n ▶️ Exacuting autorun script: $FILE \n"
