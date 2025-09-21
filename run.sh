8c8f202308a1d04d5484606c43fe4eacbc8e64a6
unknown <me@travishughes.net>
2025-09-21 08:48:12 +0100

    adding reclusive file downloading

--- run.sh
+++ run.sh
@@ -13,26 +13,22 @@ fi
 # Create Tempory Folder
 mkdir -p "$TEMP_DATA_PATH"
 
-# Download dependancies
-wget -nd -np -P "$TEMP_DATA_PATH" --recursive "$FILE_URL"
-
 # Get tmux session script
-# wget -q -P "$TEMP_DATA_PATH" "$FILE_URL/session.sh"
-
-echo "Starting Tmux Server"
-tmux start-server
+wget -q -P "$TEMP_DATA_PATH" "$FILE_URL/session.sh"
 
 
 # Create install user
 INSTALL_USER="server-deploy-kit"
 if id "$INSTALL_USER" >/dev/null 2>&1; then
-  echo "$INSTALL_USER found"
+  echo "$INSTALL_USER found, skipping."
 else
   echo "Create a password for the $INSTALL_USER user"
   echo "\n You can consider this your systems main user (outside of root of course). We will preform installs\n using this user. \n"
   sudo adduser --gecos "" --ingroup sudo "$INSTALL_USER"
 fi
 
+echo "Starting Tmux Server"
+tmux start-server
 
 # Run tmux session if not already running
 if sudo -u "$INSTALL_USER" tmux has-session -t deploy_session 2>/dev/null; then
--- session.sh
+++ session.sh
@@ -14,8 +14,8 @@ FILE_URL=https://k8s-deploy.ap-host.net
 
 
 # Download dependancies
-# wget -q -P "$TEMP_DATA_PATH" "$FILE_URL/files.txt"
-# wget -q -P "$TEMP_DATA_PATH" -i "$TEMP_DATA_PATH/files.txt" -B "$FILE_URL"
+wget -q --recursive -P "$TEMP_DATA_PATH" "$FILE_URL/files.txt"
+wget -q --recursive -P "$TEMP_DATA_PATH" -i "$TEMP_DATA_PATH/files.txt" -B "$FILE_URL"
 
 
 # Include util script
