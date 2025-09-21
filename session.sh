caf7be0157d4aefc47735a460fe022960c07069f
unknown <me@travishughes.net>
2025-09-21 08:12:44 +0100

    testing better file download method

--- session.sh
+++ session.sh
@@ -12,8 +12,10 @@ fi
 
 
 # Download dependancies
-wget -q -P "$TEMP_DATA_PATH" "$FILE_URL/files.txt"
-wget -q -P "$TEMP_DATA_PATH" -i "$TEMP_DATA_PATH/files.txt" -B "$FILE_URL"
+wget  -nd -np -P "$TEMP_DATA_PATH" --recursive "$FILE_URL"
+
+# wget -q -P "$TEMP_DATA_PATH" "$FILE_URL/files.txt"
+# wget -q -P "$TEMP_DATA_PATH" -i "$TEMP_DATA_PATH/files.txt" -B "$FILE_URL"
 
 
 # Include util script
@@ -21,7 +23,7 @@ wget -q -P "$TEMP_DATA_PATH" -i "$TEMP_DATA_PATH/files.txt" -B "$FILE_URL"
 
 
 # Load scripts from autorun.
-for FILE in "$TEMP_DATA_PATH"/*-*.sh; do
+for FILE in "$TEMP_DATA_PATH"/autorun/*.sh; do
   echo "\n ▶️ Exacuting autorun script: $FILE \n"
   . "$FILE"
 done
